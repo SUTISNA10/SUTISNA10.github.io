a95b5fed382f31df71d0330c23b4d5cf08d65241
SUTISNA10 <sutisna10chuza@gmail.com>
2021-03-07 11:19:48 +0700

    mengganti tampilan website dengan menambahkan waves, mengganti content header menjadi jumbotron, membuat content jumbotron menjadi ketengah, mengganti tampilan portfolio

--- script/main.js
+++ script/main.js
@@ -1,61 +1,52 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function(e){
-    // ambil isi href
-    var tujuan =$(this).attr('href');
+$(".page-scroll").on("click", function (e) {
+  // ambil isi href
+  var tujuan = $(this).attr("href");
 
-    // tangkap elemen
-    var elemenTujuan = $(tujuan);
+  // tangkap elemen
+  var elemenTujuan = $(tujuan);
 
-    // pindahkan scroll
-    $('html, body').animate({
-        scrollTop: elemenTujuan.offset().top - 50
-    }, 1250, 'swing');
+  // pindahkan scroll
+  $("html, body").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 50,
+    },
+    1250,
+    "swing"
+  );
 
-    e.preventDefault();
+  e.preventDefault();
 });
-
-
 
 // Parallax
 
-$(window).scroll(function(){
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
 
-    var wScroll = $(this).scrollTop();
+  // header
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 6 + "%)",
+  });
 
-    // header
-    $('header img').css({
-        'transform' : 'translate(0px, '+wScroll/6+'%)'
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + wScroll + "%)",
+  });
+
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)",
+  });
+
+  $(".jumbotron a").css({
+    transform: "translate(0px, " + wScroll + "%)",
+  });
+
+  // Portfolio
+  // design
+  if (wScroll > $(".section-portfolio").offset().top - 400) {
+    $(".design-foto .figure-img").each(function (i) {
+      setTimeout(function () {
+        $(".design-foto .figure-img").eq(i).addClass("muncul");
+      }, 200 * (i + 1));
     });
-
-    $('header h1').css({
-        'transform' : 'translate(0px, '+wScroll+'%)'
-    });
-
-    $('header p').css({
-        'transform' : 'translate(0px, '+wScroll/2+'%)'
-    });
-
-    $('header a').css({
-        'transform' : 'translate(0px, '+wScroll/1+'%)'
-    });
-
-
-    // Portfolio
-    // design
-    if (wScroll > $('.design').offset().top -400){
-        $('.design-foto .figure-img').each(function(i){
-            setTimeout(function(){
-                $('.design-foto .figure-img').eq(i).addClass('muncul');
-            }, 200 * (i+1));
-        });
-    }
-
-    // developer
-    if (wScroll > $('.developer').offset().top -380){
-        $('.developer-foto .figure-img').each(function(i){
-            setTimeout(function(){
-                $('.developer-foto .figure-img').eq(i).addClass('muncul');
-            }, 200 * (i+1));
-        });
-    }
+  }
 });
